952aac91376b8367f6de0c680b868c77dc671502
Miragitai <Ukolov2016@gmail.com>
2025-03-12 02:27:48 +0300

    push2

--- CreateVaultMananager.lua
+++ CreateVaultMananager.lua
@@ -3,8 +3,11 @@ while true do
     local size = vault.size()
     local cur = #vault.list()
     if cur >= (size*0.9) then
-        redstone.setAnalogOutput("left", 15)
+        print("I'm full! Applying redstone")
+        redstone.setAnalogOutput("left", 14)
     else
+        print("I'm fine.")
         redstone.setOutput("left", false)
     end
+    sleep(5)
 end
