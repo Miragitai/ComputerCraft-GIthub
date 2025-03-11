3378e1fee37ae7b7abfc06d0f7c2f31dd508a742
Miragitai <Ukolov2016@gmail.com>
2025-03-12 02:44:54 +0300

    fix signal

--- CreateVaultMananager.lua
+++ CreateVaultMananager.lua
@@ -1,18 +1,18 @@
 vault = peripheral.wrap("back")
 while true do
-    redstone.setOutput("right", 0)
-    redstone.setOutput("left", 0)
+    redstone.setAnalogOutput("right", 0)
+    redstone.setAnalogOutput("left", 0)
     local size = vault.size()
     local cur = #vault.list()
     if cur >= (size*0.9) then
         print("I'm full! Applying redstone")
+        redstone.setAnalogOutput("right", 15)
         redstone.setAnalogOutput("left", 15)
-        redstone.setOutput("left", true)
         sleep(60)
     else
         print("I'm fine.")
-        redstone.setOutput("right", 0)
-        redstone.setOutput("left", 0)
+        redstone.setAnalogOutput("right", 0)
+        redstone.setAnalogOutput("left", 0)
     end
     sleep(5)
 end
